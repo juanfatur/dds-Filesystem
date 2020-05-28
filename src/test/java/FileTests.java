4e2c0dc3e3439c110efea5913e2db80fc881c26a
Juan Fatur <jffatur@gmail.com>
2020-05-28 01:17:18 -0300

    Añadidos tests de escritura

--- src/test/java/FileTests.java
+++ src/test/java/FileTests.java
@@ -5,6 +5,9 @@ public class FileTests {
 
 	File file;
 	Bloque bloqueDefault;
+	Bloque c0;
+	Bloque c1;
+	Bloque c2;
 	
 	@Before
 	public void setUp() {
@@ -13,33 +16,50 @@ public class FileTests {
 		byte[] secuenciaBytes = {0,1,2,3,4,5,6,7,8,9,10};
 		bloqueDefault.setBytes(secuenciaBytes, 11);
 		file.syncWrite(bloqueDefault);
+		c0 = file.syncRead(4);
+		c1 = file.syncRead(1);
+		c2 = file.syncRead(5);
 	}
 	
 	
 	@Test
 	public void syncReadFileBloquec0() {
-		Bloque c0 = file.syncRead(4);
-		Bloque c1 = file.syncRead(1);
-		Bloque c2 = file.syncRead(5);
 		Assert.assertEquals(1,c0.getBytes()[1]);
 	}
 	
 	@Test
 	public void syncReadFileBloquec1() {
-		Bloque c0 = file.syncRead(4);
-		Bloque c1 = file.syncRead(1);
-		Bloque c2 = file.syncRead(5);
 		Assert.assertEquals(4, c1.getBytes()[0]);
 	}
 	
 	@Test
 	public void syncReadFileBloquec2() {
-		Bloque c0 = file.syncRead(4);
-		Bloque c1 = file.syncRead(1);
-		Bloque c2 = file.syncRead(5);
 		Assert.assertEquals(7, c2.getBytes()[2]);
 	}
+	
+	@Test
+	public void asyncReadFileBloquec2() {
+		Bloque c3 = file.asyncRead(1);
+		Assert.assertEquals(10, c3.getBytes()[0]);
+	}
+	
+	@Test
+	public void escribirBloquesEnArchivo() {
+		File nuevoArchivo = new FileWithMockSystem("Path archivo nuevo");
+		nuevoArchivo.syncWrite(c0);
+		Bloque nuevoBloque = new Bloque();
+		byte[] nuevosBytes = {0x0,0x10,0x0};
+		nuevoBloque.setBytes(nuevosBytes, 3);
+		nuevoArchivo.syncWrite(nuevoBloque);
+		nuevoArchivo.syncWrite(c1);
+		nuevoArchivo.syncWrite(c2);
+		byte[] bytesLeidos = nuevoArchivo.syncRead(13).getBytes();
+		byte[] bytesEsperados = {0,1,2,3,0x0,0x10,0x0,4,5,6,7,8,9};
+		Assert.assertArrayEquals(bytesEsperados, bytesLeidos);
+		
+	}
 
+	
 
 	
 	/*
